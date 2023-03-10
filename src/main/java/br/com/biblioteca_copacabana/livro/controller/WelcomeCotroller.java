ee31a175fdaeabb567296bb4cd0094bc5496e38e
danielcordeirofaria <danielcordeirofaria@gmail.com>
2023-03-10 15:56:34 -0300

    EDITING THE WELCOME MESSAGE

--- src/main/java/br/com/biblioteca_copacabana/livro/controller/WelcomeCotroller.java
+++ src/main/java/br/com/biblioteca_copacabana/livro/controller/WelcomeCotroller.java
@@ -9,10 +9,10 @@ public class WelcomeCotroller {
 	@GetMapping("/")
 	public String sayWelcome() {
 		return"<pre>"
-				+ "Bem vindo à API de livros<br/>"
-				+ "Endpoints disponíveis <br/>"
-				+ "/livros (GET) - para recuperar tdo <br/>"
-				+ "/livros/id (GET) - para recuperar detalhes de um livro<br/>"
-				+ "/livros (POST) - para incluir um novo livro</pre>";
+				+ "<h1>Bem vindo à API de livros<br/><h1/>"
+				+ "<h2>Endpoints disponíveis <br/><h2/>"
+				+ "<h2>/livros (GET) - para recuperar tdo <br/><h2/>"
+				+ "<h2>/livros/id (GET) - para recuperar detalhes de um livro<br/><h2/>"
+				+ "<h2>/livros (POST) - para incluir um novo livro</pre><h2/>";
 	}
 }
